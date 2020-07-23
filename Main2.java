4836976d1827c2984f1a5c2bf9b4c3ec68b5a6b1
pavanichunduri <navyabio12@gmail.com>
2020-07-23 15:35:14 +0530

    First Commit

---
+++ Main2.java
@@ -0,0 +1,35 @@
+﻿package junit;
+
+public class Main2
+
+{
+
+	public String remChar(String s)
+
+	{
+
+		char first=s.charAt(0);
+
+		char second=s.charAt(1);
+
+		if(first=='A')
+
+			if(second=='A')
+
+				return s.substring(2,s.length());
+
+			else
+
+				return s.substring(1,s.length());
+
+		else
+
+			if(second=='A')
+
+				return first+""+s.substring(2,s.length());
+
+		return s;
+
+	}
+
+}
---
+++ MainClass.java
@@ -0,0 +1,70 @@
+﻿package junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+
+import org.junit.Test;
+
+public class MainClass
+
+{
+
+	Main2 mainclass;
+
+	@Before
+	public void setup()
+
+	{
+
+		mainclass=new Main2();
+
+	}
+
+	@Test
+	public void first()
+
+	{
+
+		assertEquals("BCD",mainclass.remChar("ABCD"));
+
+	}
+
+	@Test
+	public void firstTwo()
+
+	{
+
+		assertEquals("CD",mainclass.remChar("AACDIJ"));
+
+	}
+
+	@Test
+	public void second()
+
+	{
+
+		assertEquals("BCD",mainclass.remChar("BACDEF"));
+
+	}
+
+
+	@Test
+	public void noChar()
+
+	{
+
+		assertEquals("BBAA",mainclass.remChar("BBAABB"));
+
+	}
+
+	@Test
+	public void nextChar()
+
+	{
+		assertEquals("BAA",mainclass.remChar("AABBB"));
+
+	}
+
+
+}
